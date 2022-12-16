bb1fd43a1df62a43f514abcdde507a45a19e8d44
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-16 18:46:19 +0000

    fixed cloudinary import in settings.py

--- christmas_blend/settings.py
+++ christmas_blend/settings.py
@@ -15,9 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
