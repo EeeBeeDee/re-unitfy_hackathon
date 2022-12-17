6a7ebcea0e40f3196125d7c69849ec8eea8b1cc8
Philip Jambrisak <jambrisak@gmail.com>
2022-12-17 16:05:04 +0100

    new navbar

--- christmas_blend/settings.py
+++ christmas_blend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['christmas-blend.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['christmas-blend.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000']
 
 
 # Application definition
