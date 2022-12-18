bcdab339a0c7c708500d8d687ce03649fb7125be
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-18 12:20:56 +0000

    add Philips navbar with css and js script

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,13 +1,11 @@
-const navMenu = document.querySelector(".nav");
-const navOverlay = document.querySelector(".nav-overlay");
-const navButton = document.querySelector(".nav-btn");
-
-navButton.addEventListener("click", () => {
-    navMenu.classlist.add("nav-open");
-    navOverlay.classlist.add("nav-overlay-open");
-});
-
-navOverlay.addEventListener("click", () => {
-    navMenu.classlist.add("nav-open");
-    navOverlay.classlist.add("nav-overlay-open");
-});
+var menuHolder = document.getElementById('menuHolder')
+var siteBrand = document.getElementById('siteBrand')
+function menuToggle(){
+  if(menuHolder.className === "drawMenu") menuHolder.className = ""
+  else menuHolder.className = "drawMenu"
+}
+if(window.innerWidth < 426) siteBrand.innerHTML = "MAS"
+window.onresize = function(){
+  if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
+  else siteBrand.innerHTML = "MY AWESOME WEBSITE"
+}
